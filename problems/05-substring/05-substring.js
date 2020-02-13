84ba8111277e104fcbc37c8a4476f060168a400a
Jesus Cabrero <cabrerojesus@gmail.com>
2020-02-13 09:14:36 -0500

    Fundamentals

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,12 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+    if(endIndex >= someStr.length || startIndex < 0)
+    {
+        throw new Error("index out of bounds")       
+    }
+    
+    return someStr.substring(startIndex,endIndex)
 }
+
+console.log(substring("Hello", 0, 3))
--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,11 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    //if someNum & 1 give you a 1 then its an even number
+    return (!(someNum&1))
+}
+
+for(let i = 0; i<10; ++i)
+{
+    console.log(isEven(i))
 }
